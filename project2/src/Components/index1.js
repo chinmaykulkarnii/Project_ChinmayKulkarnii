71fc1219a3560e1c6ab31e61cf9eb0e7a3808b21
Chinmay Kulkarni <chinmaykulkarni099@gmail.com>
2024-04-15 00:50:13 +0530

    react2

---
+++ project2/src/Components/index1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index1 = () => {
+    return (
+        <div>
+            <h2>This is Index1.js</h2>
+        </div>
+    )
+}
+
+export default index1;
---
+++ project2/src/Components/index2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index2 = () => {
+    return (
+        <div>
+            <h2>This is Index2.js</h2>
+        </div>
+    )
+}
+
+export default index2;
